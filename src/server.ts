745a0fc47c235736a194536acd917bd5de768c5b
mhzed <minhongz@gmail.com>
2017-11-29 16:20:00 +0800

    fix CORS

--- src/server.ts
+++ src/server.ts
@@ -96,7 +96,7 @@ const installXploreProxy = (app: any, hostport: string, thisport: string) : void
     // support CORS
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       if ('OPTIONS' == req.method) {
         res.sendStatus(200);
       }
